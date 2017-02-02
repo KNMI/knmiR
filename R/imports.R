c378674c904966c925f9741ccde9b519b996fb6b
MartinRoth <marsianus@gmail.com>
2017-02-02 16:46:25 +0100

    Moved KIS functionality from DutchClimate here

--- R/imports.R
+++ R/imports.R
@@ -1,5 +1,11 @@
+#' @import checkmate
+#' @import data.table
+#' @import futile.logger
 #' @importFrom methods extends as
-#' @importFrom utils read.table
-#' @importFrom stats na.omit
+#' @importFrom utils read.table download.file
+#' @importFrom stats na.omit rnorm
 #' @importFrom ncdf4 nc_open ncvar_get nc_close
+#' @importFrom xts .parseISO8601
+#' @importFrom stringr str_replace_all
+#' @importFrom magrittr '%>%'
 NULL
