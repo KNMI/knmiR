0a1ef949448717e70b328b4efd97736a54b7ffa6
MartinRoth <marsianus@gmail.com>
2017-02-03 09:10:37 +0100

    Do not test actuality of data sources on appveyor

--- tests/testthat/testActualityDataSources.R
+++ tests/testthat/testActualityDataSources.R
@@ -7,6 +7,7 @@ today <- as.Date(Sys.time())
 
 test_that("Actuality", {
   skip_on_travis()
+  skip_on_appveyor()
   expect_gt(HomogenPrecip(11, paste0(today-45, "/", today), path = NULL)[, .N], 0)
   recentQuakes <- Earthquakes("induced", NULL, paste0(today-14, "/", today), path=NULL)
   expect_gt(recentQuakes[, .N], 0)
