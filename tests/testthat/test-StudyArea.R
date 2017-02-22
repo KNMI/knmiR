d31f426eca126d31cd45dc8735ce5b51ed4832cb
MartinRoth <marsianus@gmail.com>
2017-02-22 11:44:57 +0100

    SpatialObjects should be compared via the gEqualsExact function from rgeos

--- tests/testthat/test-StudyArea.R
+++ tests/testthat/test-StudyArea.R
@@ -5,8 +5,8 @@ test_that("reference ", {
                         4.45, 4.15, 51.2, 55, 55.1, 54.9, 54.5, 52, 51.25,
                         50.75, 50.75, 51.35, 51.35, 51.2),
                       .Dim = c(12L, 2L))
-  expect_equal_to_reference(StudyArea(coords),
-                            file = "./referenceOutput/StudyArea.rds")
-  expect_equal_to_reference(StudyArea(coords, bbox=TRUE),
-                            file = "./referenceOutput/StudyAreaBbox.rds")
+  expect_true(rgeos::gEqualsExact(StudyArea(coords),
+                           readRDS("./referenceOutput/StudyArea.rds")))
+  expect_true(rgeos::gEqualsExact(StudyArea(coords, bbox = TRUE),
+                           readRDS("./referenceOutput/StudyAreaBbox.rds")))
 })
